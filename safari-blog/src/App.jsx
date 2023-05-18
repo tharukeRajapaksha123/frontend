6405c17e3b01498061a9f94a34d7ca44edde317b
Tharushi <87642831+IT21048500@users.noreply.github.com>
2023-05-18 08:40:45 +0530

    ADD:firebase added

--- safari-blog/src/App.jsx
+++ safari-blog/src/App.jsx
@@ -5,6 +5,20 @@ import { ActivityProvider } from './contexts/ActivityContext';
 import { LoadingProvider } from './contexts/LoadingContext';
 import AdminDashboard from './components/dashboard/AdminDashboard';
 import 'antd/dist/reset.css';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCb7Yo6jbsA30D0P8eQEtWLkrNAMeuQw4U",
+  authDomain: "marioblog-ad8e5.firebaseapp.com",
+  projectId: "marioblog-ad8e5",
+  storageBucket: "marioblog-ad8e5.appspot.com",
+  messagingSenderId: "134259039673",
+  appId: "1:134259039673:web:d7b32a238ef0492bbeea68"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 function App() {
 
 
--- safari-blog/src/services/file_upload_service.jsx
+++ safari-blog/src/services/file_upload_service.jsx
@@ -1,32 +1,36 @@
 // import firebase from '../firebase';
-// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 
 async function uploadImage(file, dispatch) {
-    // dispatch({
-    //     type: "SET_LOADING",
-    //     payload: true
-    // })
-    // const storageRef = firebase.storage().ref();
-    // const fileRef = storageRef.child(`images/${file.path}`);
+    const storage = getStorage();
+    console.log(imageFile.name)
+    const storageRef = ref(storage, `images/${imageFile.name}`);
 
-    // try {
-    //     const snapshot = await fileRef.put(file);
-    //     const url = await snapshot.ref.getDownloadURL();
-    //     dispatch({
-    //         type: "SET_LOADING",
-    //         payload: true
-    //     })
-    //     return url;
-    // } catch (error) {
-    //     console.error("Error uploading image: ", error);
-    //     dispatch({
-    //         type: "SET_LOADING",
-    //         payload: false
-    //     })
-    //     return null
-    // }
-    return "https://plus.unsplash.com/premium_photo-1666755275919-80c2eadbc6aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+    dispatch({
+        type: "SET_LOADING",
+        payload: true
+    })
+
+    try {
+        // Upload image to Firebase Storage
+        await uploadBytes(storageRef, imageFile);
+
+        // Get download URL of the uploaded image
+        const downloadURL = await getDownloadURL(storageRef);
+        dispatch({
+            type: "SET_LOADING",
+            payload: false
+        })
+        return downloadURL;
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        dispatch({
+            type: "SET_LOADING",
+            payload: false
+        })
+        return null
+    }
 }
 
 
